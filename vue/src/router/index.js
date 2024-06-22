3a4fc5e482a9d588835ba79fe87051edacc58d52
zhanghxiao <137479165+zhanghxiao@users.noreply.github.com>
2024-06-22 12:54:10 +0800

    Update index.js

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -15,11 +15,11 @@ const routes = [
     component: () => import('../views/Manager.vue'),
     redirect: '/home',  // 重定向到主页
     children: [
-      { path: 'home', name: 'Home', meta: { name: '系统首页' }, component: () => import('../views/manager/Home') },
+      { path: 'home', name: 'Home', meta: { name: '思维导图' }, component: () => import('../views/manager/Home') },
       { path: 'about', name: 'About', meta: { name: '关于' }, component: () => import('../views/manager/About') },
-      { path: 'home1', name: 'Home1', meta: { name: 'Home1' }, component: () => import('../views/manager/Home1') },
-      { path: 'home2', name: 'Home2', meta: { name: 'Home2' }, component: () => import('../views/manager/Home2') },
-      { path: 'home3', name: 'Home3', meta: { name: 'Home3' }, component: () => import('../views/manager/Home3') },
+      { path: 'home1', name: 'Home1', meta: { name: '流程图' }, component: () => import('../views/manager/Home1') },
+      { path: 'home2', name: 'Home2', meta: { name: '用例图' }, component: () => import('../views/manager/Home2') },
+      { path: 'home3', name: 'Home3', meta: { name: '时序图' }, component: () => import('../views/manager/Home3') },
       { path: 'home4', name: 'Home4', meta: { name: 'Home4' }, component: () => import('../views/manager/Home4') },
       { path: 'home5', name: 'Home5', meta: { name: 'Home5' }, component: () => import('../views/manager/Home5') },
     ]
